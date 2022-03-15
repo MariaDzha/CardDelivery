a97cc8a70917ae63f9cedf0cc9a42ac373917e5f
MariaDzha <m_zudina@inbox.ru>
2022-03-15 12:55:36 +0300

    tests modified

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,9 +1,13 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -12,33 +16,33 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    String generateDate(int days) {
+    return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+}
+    String deliveryDate = generateDate(3);
+
     @Test
     void shouldRegisterCardDelivery() {
 
-        Date deliveryDate = new Date();
-        SimpleDateFormat formatForDeliveryDate = new SimpleDateFormat("E dd.MM.yyyy");
-
         open ("http://localhost:9999");
-        SelenideElement form = $(By.className("form"));
         $("[Data-test-id=city] input").setValue("Саратов");
-        $("[Data-test-id=date] input").setValue(String.valueOf(deliveryDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[Data-test-id=date] input").setValue(deliveryDate);
         $("[Data-test-id=name] input").setValue("Петрова-Иванова Ольга");
         $("[Data-test-id=phone] input").setValue("+79998886677");
         $(By.cssSelector("[class='checkbox__box']")).click();
         $$("button").find(exactText("Забронировать")).click();;
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + deliveryDate), Duration.ofSeconds(15));
     }
 
     @Test
     void shouldNotRegisterWrongCity() {
 
-        Date deliveryDate = new Date();
-        SimpleDateFormat formatForDeliveryDate = new SimpleDateFormat("E dd.MM.yyyy");
-
         open ("http://localhost:9999");
-        SelenideElement form = $(By.className("form"));
         $("[Data-test-id=city] input").setValue("Париж");
-        $("[Data-test-id=date] input").setValue(String.valueOf(deliveryDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[Data-test-id=date] input").setValue(deliveryDate);
         $("[Data-test-id=name] input").setValue("Петрова-Иванова Ольга");
         $("[Data-test-id=phone] input").setValue("+79998886677");
         $(By.cssSelector("[class='checkbox__box']")).click();
@@ -48,13 +52,10 @@ public class CardDeliveryTest {
     @Test
     void shouldNotRegisterWrongName() {
 
-        Date deliveryDate = new Date();
-        SimpleDateFormat formatForDeliveryDate = new SimpleDateFormat("E dd.MM.yyyy");
-
         open ("http://localhost:9999");
-        SelenideElement form = $(By.className("form"));
         $("[Data-test-id=city] input").setValue("Москва");
-        $("[Data-test-id=date] input").setValue(String.valueOf(deliveryDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[Data-test-id=date] input").setValue(deliveryDate);
         $("[Data-test-id=name] input").setValue("Petrova Olga");
         $("[Data-test-id=phone] input").setValue("+79998886677");
         $(By.cssSelector("[class='checkbox__box']")).click();
@@ -64,13 +65,10 @@ public class CardDeliveryTest {
     @Test
     void shouldNotRegisterWrongPhone() {
 
-        Date deliveryDate = new Date();
-        SimpleDateFormat formatForDeliveryDate = new SimpleDateFormat("E dd.MM.yyyy");
-
         open ("http://localhost:9999");
-        SelenideElement form = $(By.className("form"));
         $("[Data-test-id=city] input").setValue("Москва");
-        $("[Data-test-id=date] input").setValue(String.valueOf(deliveryDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[Data-test-id=date] input").setValue(deliveryDate);
         $("[Data-test-id=name] input").setValue("Петрова Ольга");
         $("[Data-test-id=phone] input").setValue("+799988866");
         $(By.cssSelector("[class='checkbox__box']")).click();
